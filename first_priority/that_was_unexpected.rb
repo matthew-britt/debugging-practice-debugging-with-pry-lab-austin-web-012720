5a5628981f1d0c176237805786d10612d646cc53
Matthew Britt <mwbritt@hotmail.com>
2020-01-22 02:59:50 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -22,7 +22,6 @@ class Person
   def ballin_candy_lover?
     if @candy_lover == true
       "Aw sweet, this cat loves them some candy!"
-      
     else
       10.times do
         "not a ballin candy lover"
